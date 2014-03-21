759727c7d1b2bbbb14924aa1c21025e079a90531
Isaac IY Saito <130s@lateeye.net>
2014-03-21 17:20:41 +0900

    Fix #2

--- demo/RTSE.bat
+++ demo/RTSE.bat
@@ -1,3 +1,5 @@
-cd ..\misc\OpenRTM-aist\1.1\tools
+set JAVA_HOME=..\misc\openjdk_1.7.0_u45_2.4.3_installed
+set PATH=;..\misc\openjdk_1.7.0_u45_2.4.3_installed\bin;..\misc\openjdk_1.7.0_u45_2.4.3_installed\jre\bin;%PATH%
 
+cd ..\misc\OpenRTM-aist\1.1\tools
 RTSystemEditorRCP.exe
