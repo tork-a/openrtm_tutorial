95c4e9a79ff7d3cefe55d5e0f991e21da89ea837
Isaac IY Saito <130s@lateeye.net>
2014-03-30 09:03:57 -0500

    Fix #10

--- demo/SampleWordLogger/0_StartDemo.bat
+++ demo/SampleWordLogger/0_StartDemo.bat
@@ -1,3 +1,12 @@
+REM ''' Usage of this file
+REM %1 usb drive to be used
+REM '''
+
+set usbdrive=%1
+%usbdrive%
+cd demo\SampleWordLogger
+echo Moved to DIR=%cd%
+
 rem setup SampleWordLogger
 cd ..\SampleWordLogger\build\src\Debug
 call SampleWordLoggerComp.exe
--- demo/SampleWordLogger/4_DisconnectRTC.bat
+++ demo/SampleWordLogger/4_DisconnectRTC.bat
@@ -1,3 +1,12 @@
+REM ''' Usage of this file
+REM %1 usb drive to be used
+REM '''
+
+set usbdrive=%1
+%usbdrive%
+cd demo\SampleWordLogger
+echo Moved to DIR=%cd%
+
 for /f "usebackq" %%h in (`hostname`) do set HOSTNAME=%%h
 
 call ..\Apps\rtshell\rtdis 127.0.0.1/%HOSTNAME%.host_cxt/SampleWordLogger0.rtc
