ef488910259328873bf00e0927311ca18a115f45
Isaac IY Saito <130s@lateeye.net>
2014-04-15 01:01:53 +0900

    (demo) fix conflct

--- demo/MotionByVoiceDemo/0_StartDemo.bat
+++ demo/MotionByVoiceDemo/0_StartDemo.bat
@@ -2,15 +2,14 @@ REM ''' Usage of this file
 REM %1 usb drive to be used
 REM '''
 
+set RTM_ROOT=%usbdrive%\openrtm_tutorial\misc\OpenRTM-aist\1.1
+set OHRI_ROOT=%usbdrive%\openrtm_tutorial\demo\OpenHRIAudio
+set PATH=%PATH%;%RTM_ROOT%\runtime;%OHRI_ROOT%;%OMNI_ROOT%\bin\x86_win32
+
 set usbdrive=%1
 %usbdrive%
 cd openrtm_tutorial\demo\MotionByVoiceDemo
 echo Moved to DIR=%cd%
-
-echo CURRENT DIR=%cd%
-set usbdrive=%2
-if NOT DEFINED usbdrive set usbdrive=F:
-cd /F %usbdrive%\   REM Changing directory to the USB drive.
 
 rem These processes are added by trial to mimic "0 SpeechDemo"
 start ..\OpenHRIAudio\portaudioinput.exe
