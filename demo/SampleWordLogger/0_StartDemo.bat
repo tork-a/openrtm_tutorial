4c3007b5fdaa0791203baaf3239d32613578be16
U-WIN-MRORKVT5AAL\Isaac Saito <Isaac Saito@WIN-MRORKVT5AAL.(none)>
2014-04-15 00:34:28 +0900

    (demo) Fix 2.1 tutorial path

--- demo/SampleWordLogger/0_StartDemo.bat
+++ demo/SampleWordLogger/0_StartDemo.bat
@@ -2,6 +2,9 @@ REM ''' Usage of this file
 REM %1 usb drive to be used
 REM '''
 
+set RTM_ROOT=%usbdrive%\openrtm_tutorial\misc\OpenRTM-aist\1.1
+set PATH=%PATH%;%RTM_ROOT%\runtime;%RTM_ROOT%\bin;%RTM_ROOT%\omniORB\4.1.5\bin\x86_win32
+
 set usbdrive=%1
 %usbdrive%
 cd openrtm_tutorial\demo\SampleWordLogger
