8bcce78be30a38334dbd64756ae72f70bc5e271b
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2015-11-02 12:16:44 -0800

    [OpencvRtmDemo/0_StartDemo.bat] Use common variable

--- demo/OpencvRtmDemo/0_StartDemo.bat
+++ demo/OpencvRtmDemo/0_StartDemo.bat
@@ -4,10 +4,11 @@ if NOT DEFINED usbdrive set usbdrive=F:
 cd /d %usbdrive%\
 
 cd openrtm_tutorial/misc/OpenRTM-aist/1.1/examples/C++/opencv-rtcs
-set OMNI_ROOT=%usbdrive%\openrtm_tutorial\misc\OpenRTM-aist\1.1\omniORB\4.1.5
-set RTM_ROOT=%usbdrive%\openrtm_tutorial\misc\OpenRTM-aist\1.1
-set OPENCV_ROOT=%usbdrive%\openrtm_tutorial\demo\Rtc_CvMonitor
-rem set PATH=%PATH%;%OMNI_ROOT%\bin\x86_win32;%usbdrive%\openrtm_tutorial\misc\OpenRTM-aist\1.1\examples\C++\opencv-rtcs
+set TUTORIAL_ROOT=%usbdrive%\openrtm_tutorial
+set OMNI_ROOT=%TUTORIAL_ROOT%\misc\OpenRTM-aist\1.1\omniORB\4.1.5
+set RTM_ROOT=%TUTORIAL_ROOT%\misc\OpenRTM-aist\1.1
+set OPENCV_ROOT=%TUTORIAL_ROOT%\demo\Rtc_CvMonitor
+rem set PATH=%PATH%;%OMNI_ROOT%\bin\x86_win32;%TUTORIAL_ROOT%\misc\OpenRTM-aist\1.1\examples\C++\opencv-rtcs
 set PATH=%PATH%;%RTM_ROOT%\runtime;%OPENCV_ROOT%;%OMNI_ROOT%\bin\x86_win32;%usbdrive%\misc\OpenRTM-aist\1.1\examples\C++\opencv-rtcs
 
 echo CURRENT DIR=%cd%
